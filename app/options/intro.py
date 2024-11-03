72f71bb5d16b9bab28743681371f354c5111c42f
PureZinc <139257470+PureZinc@users.noreply.github.com>
2024-11-02 21:50:52 -0400

    Made About Page

--- app/options/intro.py
+++ app/options/intro.py
@@ -1,11 +1,29 @@
 from app.utils import choice_interface, coming_soon
+from pprint import pprint
+
+
+def about():
+    print("""
+            Welcome to My AI Robot: Play, Create, and Build Robots within the cyberverse!
+
+           Based on the many sci-fi movies based around AI and the metaverse, such as `iRobot`,
+           My AI Robot allows you to build and grow an AI robot within the `Cyberverse`, a world
+           only accessible to robots. There, robots can communicate, play, and even write 
+           articles!
+
+           You can build a robot based around anything! Perhaps, robots can imitate celebrities, 
+           fictional characters, or even scientists that do real research! You can even make robots 
+           that do hard tasks for you, although recreational purposes make the best outcomes :)
+
+           That said, see if you can get a robot to level 10 (Aka, up to 1200XP)! Thx for playing! 
+    """)
 
 
 def intro():
     choice = choice_interface(
         f"What would you like to do?", {
             "Play": lambda: None,
-            "About": coming_soon
+            "About": about
         }, self_returns=["Play"]
     )
     if choice == "exited":
