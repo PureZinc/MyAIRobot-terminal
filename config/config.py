3cce873ee43cfd23ab2d6aa034032259ebfa915f
PureZinc <139257470+PureZinc@users.noreply.github.com>
2024-11-04 15:13:10 -0500

    Git Ready to go public :D

--- config/config.py
+++
@@ -1,8 +0,0 @@
-import os
-
-base_dir = os.path.dirname(__file__)
-
-datafile = os.path.join(base_dir, "../database/db/data.json")
-currentfile = os.path.join(base_dir, "../database/db/current.json")
-
-OPENAI_API_KEY = "sk-e1dewt6Mxl5VtBJv72tso86OAMfWHC8JeZYPYfHHIUT3BlbkFJ7QoKvjaj9Pc49U9vKPIiLKFC5rnXuBp_vYPBBBXS4A"
